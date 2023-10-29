79327d8eb5df37c44ab3c0507b23283c705ca75e
Payal-Palve <128684645+Payal-Palve@users.noreply.github.com>
2023-10-29 19:50:21 +0530

    Loops pattern 29oct

---
+++ loopNumPattern_1Reverse.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  
+  for(int i=4;i>=1;i--)
+    {
+      for(int j=4;j>=1;j--)
+        {
+          
+          printf("\t %d",j);
+  
+        }
+     
+      printf("\n");
+    }
+  
+  
+}
